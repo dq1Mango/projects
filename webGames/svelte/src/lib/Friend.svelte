a4aadb2afe9ad077c8bf6224dacab8ee5196a31c
Thomas Ranney <thomas@ranney.com>
2025-08-14 16:21:33 -0400

    getting htm-ly / svelt-y

---
+++ webGames/svelte/src/lib/Friend.svelte
@@ -0,0 +1,24 @@
+<script>
+  let {name, presence, duration} = $props()
+</script>
+
+<div class="flex-container">
+  <h3>{name}</h3>
+  <p>{presence}, {duration}</p>
+</div>
+
+<style>
+  h3 {
+    color: var(--text)
+  }
+  p {color: var(--flamingo)}
+
+  .flex-container {
+    outline-width: 5px;
+    outline-style: solid;
+    outline-color: var(--maroon);
+
+    margin: 10px ;
+    padding-left: 10px;
+}
+</style>
