e1d840f98ef6e84b134e74bc5fa5f383f63fb9eb
Thomas Ranney <thomas@ranney.com>
2025-10-22 11:48:16 -0400

    expirmenting with modularizing the parts of the elm architecture

---
+++ signal-tui/src/update.rs
@@ -0,0 +1,66 @@
+use crate::logger::Logger;
+use crate::*;
+use color_eyre;
+use crossterm::event::{self, Event, KeyCode};
+
+#[derive(PartialEq)]
+pub enum Action {
+  Type(char),
+  Backspace,
+
+  SetMode(Mode),
+
+  Quit,
+}
+/// Convert Event to Action
+///
+/// We don't need to pass in a `model` to this function in this example
+/// but you might need it as your project evolves
+///
+/// (the project evolved (pokemon core))
+pub fn handle_event(model: &Model) -> color_eyre::Result<Option<Action>> {
+  if event::poll(Duration::from_millis(250))? {
+    if let Event::Key(key) = event::read()? {
+      if key.kind == event::KeyEventKind::Press {
+        return Ok(handle_key(key, model));
+      }
+    }
+  }
+  Ok(None)
+}
+
+pub fn handle_key(key: event::KeyEvent, model: &Model) -> Option<Action> {
+  match model.mode {
+    Mode::Insert => match key.code {
+      KeyCode::Esc => Some(Action::SetMode(Mode::Normal)),
+      KeyCode::Char(char) => Some(Action::Type(char)),
+      // this will not get confusing trust
+      KeyCode::Backspace => Some(Action::Backspace),
+      _ => None,
+    },
+    Mode::Normal => match key.code {
+      KeyCode::Char('i') => Some(Action::SetMode(Mode::Insert)),
+      // KeyCode::Char('k') => Some(Action::Decrement),
+      KeyCode::Char('q') => Some(Action::Quit),
+      _ => None,
+    },
+  }
+}
+
+pub fn update(model: &mut Model, msg: Action, logger: &mut Logger) -> Option<Action> {
+  match msg {
+    Action::Type(char) => {
+      model.current_chat().text_input.insert_char(char, logger);
+    }
+    Action::Backspace => model.current_chat().text_input.delete_char(),
+
+    Action::SetMode(new_mode) => model.mode = new_mode,
+
+    Action::Quit => {
+      // You can handle cleanup and exit here
+      // -- im ok thanks tho
+      model.running_state = RunningState::OhShit;
+    }
+  };
+  None
+}
