d49b72f74ad387daad52854c9ffc958a0342f38a
thomas <thomas@ranney.com>
2026-08-26 13:52:46 -0400

    bam directory created and opened

--- claudecode/src/main.zig
+++ claudecode/src/main.zig
@@ -1,17 +1,63 @@
 const std = @import("std");
+
 const httpz = @import("httpz");
+
+const APP_NAME = "zigclaude";
+const CREDS_FILE = "creds.json";
+
+const Creds = struct { token: []const u8, refresh: []const u8, expires: std.time.epoch };
+
+const App = struct {
+    credsDir: std.Io.Dir,
+    gpa: std.mem.Allocator,
+    io: std.Io,
+    creds: ?Creds,
+
+    fn init(gpa: std.mem.Allocator, io: std.Io, home: []const u8) !App {
+        const path = std.Io.Dir.path;
+        const homeDir = try std.Io.Dir.openDirAbsolute(io, home, std.Io.Dir.OpenOptions{});
+
+        const credsPath = try path.join(gpa, &.{ ".local", "state", APP_NAME });
+        try std.Io.Dir.createDirPath(homeDir, io, credsPath);
+
+        const dir = try homeDir.openDir(io, credsPath, std.Io.Dir.OpenOptions{
+            .follow_symlinks = false,
+        });
+
+        return App{
+            .creds = null,
+            .credsDir = dir,
+            .io = io,
+            .gpa = gpa,
+        };
+    }
+
+    fn readCreds(self: App) !void {
+        // const file = try self.credsDir.openFile(self.io, CREDS_FILE, std.Io.Dir.OpenFileOptions{ .mode = .read_write });
+        const contents = try self.credsDir.readFileAlloc(self.io, CREDS_FILE, 1024 * 1024);
+        const creds = try std.json.parseFromSlice(Creds, self.gpa, contents, std.json.ParseOptions{});
+        self.creds = creds;
+    }
+
+    // fn writeCreds(self: App) !void {}
+
+    fn refreshCreds() void {}
+};
 
 pub fn main(init: std.process.Init) !void {
     const allocator = init.gpa;
     const addr = httpz.Config.Address.localhost(5882);
 
+    const home = try init.minimal.environ.getAlloc(init.gpa, "HOME");
+    var app = try App.init(init.gpa, init.io, home);
+
     // More advanced cases will use a custom "Handler" instead of "void".
     // The last parameter is our handler instance; since we have a "void"
     // handler, we passed a void ({}) value.
-    var server = try httpz.Server(void).init(init.io, allocator, .{
+    var server = try httpz.Server(*App).init(init.io, allocator, .{
         // use .all(5882) to bind to all interfaces, i.e. 0.0.0.0
         .address = addr,
-    }, {});
+    }, &app);
     defer {
         // clean shutdown, finishes serving any live requests
         server.stop();
@@ -44,7 +90,7 @@ pub fn main(init: std.process.Init) !void {
     try server.listen();
 }
 
-fn getUser(req: *httpz.Request, res: *httpz.Response) !void {
+fn getUser(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
     res.status = 200;
     try res.json(.{ .id = req.param("id").?, .name = "Teg" }, .{});
 }
